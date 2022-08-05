1ee0b677de02e77ed589f49613fcf04d19042595
kawsermorshed5 <107338713+kawsermorshed5@users.noreply.github.com>
2022-08-05 13:03:45 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,15 +5,20 @@ $(document).ready(function () {
 
 
     // for hotel
-    $('#hotel').click(function ()){
+    $('#hotel').click(function () {
         $('.hotel-item').show();
         $('.ticket-item').hide();
         $('.place-item').hide();
-    };
+    });
     // for ticket
-    $('#ticket').click(function ()){
+    $('#ticket').click(function () {
+        $('.hotel-item').hide();
+        $('.ticket-item').show();
+        $('.place-item').hide();
+    });
+    $('#place').click(function () {
         $('.hotel-item').hide();
         $('.ticket-item').hide();
         $('.place-item').show();
-    };
+    });
 })
